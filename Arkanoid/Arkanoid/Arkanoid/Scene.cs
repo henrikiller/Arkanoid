d8b6747c373cdd81866c2ef0ecaa554f8a9041f9
Luiz Henrique Sacramento <LuizSacramento.nave@gmail.com>
2014-10-10 10:35:52 -0300

    Padrao
    padrao com as classes prontos, quase todas.

---
+++ Arkanoid/Arkanoid/Arkanoid/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Arkanoid
+{
+    abstract class Scene
+    {
+        private abstract void Draw(Graphics g);
+        private abstract void Update();
+        private abstract void KeyPressed(EventArgs e);
+        private abstract void KeyUnpressed(EventArgs e);
+    }
+}
